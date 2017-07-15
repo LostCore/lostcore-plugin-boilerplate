57fe101468c2eab8b12a69a04082bfa795d2b47c
LostCore <lostcore@gmail.com>
2017-07-15 13:39:43 +0200

    Ref

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -2,21 +2,8 @@
 
 namespace PluginName;
 
-use PluginName\includes\Activator;
-use PluginName\includes\Deactivator;
-use PluginName\includes\Plugin;
-
 /**
  * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           PluginName
  *
  * @wordpress-plugin
  * Plugin Name:       WordPress Plugin Boilerplate
@@ -32,6 +19,9 @@ use PluginName\includes\Plugin;
  */
 
 // If this file is called directly, abort.
+use PluginName\includes\i18n;
+use PluginName\includes\Loader;
+
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
@@ -64,24 +54,6 @@ spl_autoload_register( function($class){
 });
 
 /*
- * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
- */
-register_activation_hook( __FILE__, function(){ Activator::activate(); } );
-
-/*
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
- */
-register_deactivation_hook( __FILE__, function(){ Deactivator::deactivate(); } );
-
-/*
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
  */
-(new Plugin('plugin-name'))->run();
+(new PluginName(__FILE__, new Loader(__DIR__), new i18n()))->run();
